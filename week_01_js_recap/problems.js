5a8a0f479ccdb28672f2dc37cd50aa7d07dc8db9
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-25 22:59:27 -0400

    Updated week_01_js_recap completed week 1

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,16 +1,17 @@
 // Reverse a string
 function reverseString(str) {
-
+  return str.split('').reverse().join('');
 }
   
 // Find max in array
 function findMax(arr) {
-
+  if (arr.length === 0) return undefined;
+  return Math.max(...arr);
 }
 
 // Check if palindrome
 function isPalindrome(word) {
-    
+  return word === word.split('').reverse().join('');
 }
 
 module.exports = { reverseString, findMax, isPalindrome };
